7c4fa73413f34bc20dbcecf76bd2c806285e1baf
mrclkthl <mirco@lieker-thoele.com>
2024-11-16 21:43:29 +0100

    Beispielaufgaben für alle Quadranten

--- main.js
+++ main.js
@@ -8,4 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Beispielhafte Aufgaben hinzufügen
     addTask("list-urgent-important", "Beispielaufgabe 1: Sofort erledigen");
     addTask("list-not-urgent-important", "Beispielaufgabe 2: Planen");
+    addTask("list-urgent-not-important", "Beispielaufgabe 3: Planen");
+    addTask("list-not-urgent-not-important", "Beispielaufgabe 4: Planen");
 });
